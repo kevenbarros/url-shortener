f951c5fdf3eda04a1e68b35350cc784b142e1038
keven william silva barros <69265078+kevenbarros@users.noreply.github.com>
2023-04-01 19:10:02 -0300

    build

--- server/src/routes/urlRoute.ts
+++ server/src/routes/urlRoute.ts
@@ -3,7 +3,7 @@ import { postUrl, urlNavigate, getAllUrl, getUrlUser, deleteUrl } from "../contr
 import authMiddleware from "../middleware/authMiddleware"
 
 function urlRoute(router: Router): void {
-  router.get("/urlGetAll", getAllUrl);
+  router.get("/urlGetAll", getAllUrl)
   router.get("/:shortUrlId", urlNavigate);
   router.get("/urlUser/:userId", getUrlUser);
   router.post("/url", postUrl);
